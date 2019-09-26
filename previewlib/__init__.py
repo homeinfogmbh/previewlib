36cfe7296d1b237abbd4497f6b37bbafd9f36794
Richard Neumann <r.neumann@homeinfo.de>
2019-09-26 14:19:16 +0200

    Added APPLICATION.

--- previewlib/__init__.py
+++ previewlib/__init__.py
@@ -5,9 +5,11 @@ from previewlib.orm import TOKEN_TYPES
 from previewlib.orm import DeploymentPreviewToken
 from previewlib.orm import FileAccessToken
 from previewlib.orm import GroupPreviewToken
+from previewlib.wsgi import APPLICATION
 
 
 __all__ = [
+    'APPLICATION',
     'TOKEN_TYPES',
     'preview',
     'file_preview',
