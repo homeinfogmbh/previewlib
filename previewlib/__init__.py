20fdec410772f53621438b77fe7e8d696e96d330
Richard Neumann <mail@richard-neumann.de>
2021-01-29 15:17:30 +0100

    Orederd imports.

--- previewlib/__init__.py
+++ previewlib/__init__.py
@@ -1,7 +1,7 @@
 """Preview library."""
 
 from previewlib.decorators import preview, file_preview
-from previewlib.functions import make_response
+from previewlib.functions import Response, make_response
 from previewlib.orm import TOKEN_TYPES
 from previewlib.orm import DeploymentPreviewToken
 from previewlib.orm import FileAccessToken
@@ -12,10 +12,11 @@ from previewlib.wsgi import APPLICATION
 __all__ = [
     'APPLICATION',
     'TOKEN_TYPES',
-    'preview',
-    'file_preview',
-    'make_response',
     'DeploymentPreviewToken',
     'FileAccessToken',
-    'GroupPreviewToken'
+    'GroupPreviewToken',
+    'Response',
+    'file_preview',
+    'make_response',
+    'preview'
 ]
